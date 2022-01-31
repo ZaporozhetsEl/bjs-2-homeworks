d3d3b21d264802c8cf86af33d03a97f093f3aacf
Elena Zaporozhets <zaporozhetsel@gmail.com>
2022-01-31 14:04:32 +0700

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b**2 - 4 * a * c;
+  let arr = [];
+  let x1;
+  let x2;
+
+  if (d > 0) {
+      x1 = (-b + Math.sqrt(d)) / 2 * a;
+      x2 = (-b - Math.sqrt(d)) / 2 * a;
+      arr.push(x1,x2);
+  } else if (d === 0) {
+      x1 = (-b + Math.sqrt(d)) / 2 * a;
+      arr.push(x1);
+  };
+ 
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
-
   return totalAmount;
+  // код для задачи №2 писать здесь
 }
+
+
